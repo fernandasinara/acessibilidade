044465626e83f1c8f67dc56275ead82c1f9f4227
fernandasinara <141151372+fernandasinara@users.noreply.github.com>
2025-03-17 10:38:45 -0300

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener ('DOMContentLoaded',function(){
+    const aumentarFontebotao = document.getElementById('aumentar-fonte');
+    const diminuifontebotao = document.getElementById (dim)
+}
+)
